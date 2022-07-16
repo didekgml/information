4e38d6fc9d2c1435c689fa5f22d58cede126a0eb
didekgml <109408333+didekgml@users.noreply.github.com>
2022-07-16 16:21:16 +0900

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://www.instagram.com/ekgml_/" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -21,19 +21,19 @@ def information(info):
     write("b" + str(i), info[key[i]])
 
 # 배경 색깔 설정
-colors = ["green", "blue"]
+colors = ["black", "black"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "양다희")
+write("description", "다산중학교")
+write("button", "인스타그램")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "제목1": "내용1",
-  "제목2": "내용2",
-  "제목3": "내용3",
-  "제목4": "내용4"
+  "생년월일": "2007년 6월 5일",
+  "좋아하는 과목": "수학,과학",
+  "싫어하는 과목": "국어",
+  "좋아하는 것": "애플"
 }
 information(informations)
